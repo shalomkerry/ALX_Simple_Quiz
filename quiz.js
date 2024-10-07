53c45a2fda2d976fcdf1e8444c172bb7557b3d54
Shalom <youremail@gmail.com>
2024-10-08 00:03:33 +0300

    setting up everything

---
+++ quiz.js
@@ -0,0 +1,16 @@
+const button = document.querySelector("#submit-answer");
+let report = document.querySelector("#feedback");
+function checkAnswer() {
+  let correctAnswer = "4";
+  const radio = document.querySelector("input[name]:checked");
+  let useranswer = radio.value;
+
+  if (radio) {
+    if (useranswer == correctAnswer) {
+      report.textContent = "Correct! Well Done.";
+    } else {
+      report.textContent = "That is incorrect. Try Again!gh";
+    }
+  }
+}
+button.addEventListener("click", checkAnswer);
