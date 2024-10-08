3417aae872c20158064818a543a38f6460e5f456
shalomkerry <152377259+shalomkerry@users.noreply.github.com>
2024-10-08 22:08:48 +0300

    Update quiz.js

--- quiz.js
+++ quiz.js
@@ -3,10 +3,10 @@ let report = document.querySelector("#feedback");
 function checkAnswer() {
   let correctAnswer = "4";
   const radio = document.querySelector("input[name]:checked");
-  let useranswer = radio.value;
+  let userAnswer = radio.value;
 
   if (radio) {
-    if (useranswer == correctAnswer) {
+    if (userAnswer == correctAnswer) {
       report.textContent = "Correct! Well Done.";
     } else {
       report.textContent = "That is incorrect. Try Again!";
